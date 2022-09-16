8b8c31501d60af3f3af11069747362397c465c34
edithfirja <88878514+edithfirja@users.noreply.github.com>
2022-09-16 21:50:46 +0700

    Add files via upload

---
+++ volume balok.py
@@ -0,0 +1,7 @@
+#Menghitung Volume Balok
+print('Program menghitung volume balok')
+p =float (input('masukan p balok: '))
+l =float (input('masukan l balok: '))
+t =float (input('masukan t balok: '))
+hasil = p*l*t
+print(" Volume balok adalah : "+ str(hasil))
---
+++ volume kubus.py
@@ -0,0 +1,6 @@
+#Menghitung Volume kubus
+print ("Program Perhitungan Volume Kubus")
+sisi = float(input(" Masukkan Sisi: "))
+hasil = sisi+sisi+sisi
+print(" Volume Kubus adalah : "+ str(hasil))
+
